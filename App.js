68ac85a9c3af95d9b9e3eb8f99dade42792001a5
Nicholas Veras <nicholasveras@MacBook-Air-de-Nicholas.local>
2023-12-12 13:18:47 -0300

    finished interface

--- App.js
+++ App.js
@@ -1,10 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image
+        source={require('./src/crono.png')}
+      />
+
+      <Text style={styles.timer}> 00:00:00 </Text>
+
+      <View style={styles.btnArea}>
+        <TouchableOpacity style={styles.btn}>
+          <Text style={styles.btnTexto}>INICIAR!</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.btn}>
+          <Text style={styles.btnTexto}>LIMPAR</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.areaUltima}>
+        <Text style={styles.textoCorrida}>Último tempo 00:30:00</Text>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +32,42 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#00aeef',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  timer: {
+    marginTop: -160,
+    fontSize: 45,
+    fontWeight: 'bold',
+    color: '#FFF'
+  },
+  btnArea: {
+    flexDirection: 'row',
+    marginTop: 130,
+    height: 40,
+    marginBottom: 40, // Add margin at the bottom to separate from the next section
+  },
+  btn: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFF',
+    height: 40,
+    margin: 17,
+    borderRadius: 10,
+  },
+  btnTexto: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#00aeef',
+  },
+  areaUltima: {
+    marginBottom: 25, // Add margin at the top to separate from the previous section
+  },
+  textoCorrida: {
+    fontSize: 23,
+    color: '#FFF',
+    fontStyle: 'italic'
+  }
 });
