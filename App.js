9fb55217c4c8dea66e68e19229b44c8c3026592f
Nicholas Veras <nicholasveras@MacBook-Air-de-Nicholas.local>
2023-12-12 19:55:27 -0300

    project completed

--- App.js
+++ App.js
@@ -1,30 +1,93 @@
+import React, { useState } from 'react'
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 
+let timer = null;
+let segundos = 0;
+let minutos = 0;
+let horas = 0;
+
 export default function App() {
+  const [numero, setNumero] = useState(0);
+  const [botao, setBotao] = useState('INICIAR!');
+  const [ultimo, setUltimo] = useState(null);
+
+  function iniciar(){
+
+    if(timer !== null){
+      //Aqui vai parar o timer
+       clearInterval(timer);
+       timer = null;
+       setBotao('INICIAR!')
+    }else {
+      // comecar a girar o timer
+      timer = setInterval(()=>{
+        segundos++;
+
+        if(segundos == 60){
+          segundos = 0;
+          minutos++; 
+        }
+
+        if(minutos == 60){
+          minutos = 0;
+          horas++;
+        }
+
+        let format =
+        (horas < 10 ? '0' + horas : horas) + ':'
+        + (minutos < 10 ? '0' + minutos : minutos) + ':'
+        + (segundos < 10 ? '0' + segundos : segundos);
+        
+        setNumero(format); 
+
+      }, 1000);
+      setBotao('PARAR');
+    }
+  }
+
+  function limpar(){
+    if(timer !== null){
+      //parar o timer
+      clearInterval(timer);
+      timer = null;
+    }
+
+    setUltimo(numero);
+    setNumero(0);
+    segundos = 0;
+    minutos = 0;
+    horas = 0;
+    setBotao('INICIAR!')
+  }
+
   return (
     <View style={styles.container}>
+
+    <StatusBar style="auto" />
+
       <Image
         source={require('./src/crono.png')}
       />
 
-      <Text style={styles.timer}> 00:00:00 </Text>
+      <Text style={styles.timer}> {numero} </Text>
 
       <View style={styles.btnArea}>
-        <TouchableOpacity style={styles.btn}>
-          <Text style={styles.btnTexto}>INICIAR!</Text>
+        <TouchableOpacity style={styles.btn} onPress={iniciar}>
+          <Text style={styles.btnTexto}>{botao}</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity style={styles.btn}>
+        <TouchableOpacity style={styles.btn} onPress={limpar}>
           <Text style={styles.btnTexto}>LIMPAR</Text>
         </TouchableOpacity>
       </View>
 
       <View style={styles.areaUltima}>
-        <Text style={styles.textoCorrida}>Último tempo 00:30:00</Text>
+        <Text style={styles.textoCorrida}>
+          { ultimo ? 'Ultimo tempo:' + ultimo : ''}
+        </Text>
       </View>
 
-      <StatusBar style="auto" />
     </View>
   );
 }
